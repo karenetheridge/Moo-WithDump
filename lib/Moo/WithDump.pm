09b636c0ec437260284b4f92421c8809be405701
Karen Etheridge <ether@cpan.org>
2015-11-30 11:53:14 -0800

    initial commit
    via [Git::Init] in Dist-Zilla-PluginBundle-Author-ETHER-0.108, github profile.

---
+++ lib/Moo/WithDump.pm
@@ -0,0 +1,41 @@
+use strict;
+use warnings;
+package Moo::WithDump;
+# ABSTRACT: ...
+# KEYWORDS: ...
+# vim: set ts=8 sts=4 sw=4 tw=115 et :
+
+our $VERSION = '0.001';
+
+
+1;
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Moo::WithDump;
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 FUNCTIONS/METHODS
+
+=head2 C<foo>
+
+...
+
+=head1 ACKNOWLEDGEMENTS
+
+...
+
+=head1 SEE ALSO
+
+=for :list
+* L<foo>
+
+=cut
---
+++ t/01-basic.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+
+use Test::More;
+use if $ENV{AUTHOR_TESTING}, 'Test::Warnings';
+use Moo::WithDump;
+
+fail('this test is TODO!');
+done_testing;
